5119c991241e5709be8a984c03c4f8c3550aca0c
Mariam Elashram <Mariam.Elashram@Qcentriseg.com>
2025-03-21 19:43:04 +0200

    adding servers

--- bin/RunServer_Dynamic_Master.sh
+++ bin/RunServer_Dynamic_Master.sh
@@ -10,14 +10,14 @@ JTL_FILE="$BASE_DIR/results.jtl"
 REPORT_DIR="$BASE_DIR/web"
 
 # Define the remote server IP (you can change this or pass it as an argument)
-#REMOTE_HOST="10.0.36.159,10.0.37.4,10.0.39.176,10.0.46.14,10.0.46.117"
+REMOTE_HOST="10.0.46.9,10.0.34.153,10.0.44.143,10.0.36.63"
 
 # Create directories
 mkdir -p "$BASE_DIR"
 mkdir -p "$REPORT_DIR"
 
 # Run JMeter with the remote server specified and additional parameters
-./jmeter -n -t "$JMX_FILE" -l "$JTL_FILE" -e -o "$REPORT_DIR"
+./jmeter -n -t "$JMX_FILE" -r -l "$JTL_FILE" -e -o "$REPORT_DIR" -Dremote_hosts="$REMOTE_HOST"
 
 # Check if the JMeter test ran successfully
 if [ $? -eq 0 ]; then
